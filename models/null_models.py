2e2ade49d0afc2474d92411841d98067f07d6a7b
samfway <samfway@gmail.com>
2015-08-31 17:58:18 -0600

    Null models, together at last

---
+++ models/null_models.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+__author__ = "Sam Way"
+__copyright__ = "Copyright 2014, The Clauset Lab"
+__license__ = "BSD"
+__maintainer__ = "Sam Way"
+__email__ = "samfway@gmail.com"
+__status__ = "Development"
+
+
+import numpy as np
+
+
+""" All models expect similar inputs and return output in the same format.
+
+    Inputs:
+        `candidates' - a list of faculty profile objects (parse/faculty_parser.py)
+        `positions' - a list of insitution names with open jobs
+        `school_info' - a dictionary of institution profiles (parse/institution_parser.py)
+        `ranking' - which ranking to use
+
+    Returns:
+        A list of tuples, denoting candidate+position pairs. This is the list of hires made.
+"""
+
+# -------------------
+# CONFIGURATION MODEL 
+# -------------------
+class ConfigurationModel:
+    def __init__(self):
+        pass
+    
+    def simulate_hiring(self, candidates, positions, school_info, ranking='pi_inv'):
+        """ All candidates have an equal chance of being hired to each job """ 
+        random_candidates = np.random.permutation(candidates)
+        return zip(random_candidates, positions)
+
+
+# -------------------
+# ``BEST-FIRST" MODEL 
+# -------------------
+class BestFirstModel:
+    def __init__(self):
+        pass
+
+    def simulate_hiring(self, candidates, positions, school_info, ranking='pi_inv'):
+        """ Simulate faculty hiring under the 'best-first' model.
+            Under this model, the highest-ranked job is filled first,
+            and they blindly pick the candidate from the highest-ranked
+            school (regardless of gender, productivity, etc).
+
+            Assumptions: 
+            Unranked schools are effectively tied for last place
+            A small amount of noise is added to their ranking to serve as a tie-breaker. 
+            Haven't heard of that school? We'll assume the hiring committee hasn't either, 
+            and you get no bonus points for prestige.
+        """
+        worst_ranking = school_info['UNKNOWN'][ranking]
+
+        # Populate list of available candidates
+        candidate_pool = []
+        noise = 0.1 * np.random.randn(len(candidates))
+        for i, f in enumerate(candidates:
+            place, year = f.phd()
+            try:
+                rank = school_info[place][ranking]
+            except:
+                rank = worst_ranking
+            candidate_pool.append((rank+noise[i], f))
+
+        # Populate list of open jobs
+        job_pool = []
+        noise = 0.1 * np.random.randn(len(positions))
+        for i, s in enumerate(positions):
+            try:
+                rank = school_info[s][ranking]
+            except:
+                rank = worst_ranking
+            job_pool.append((rank+noise[i], s))
+
+        # Sort both lists and zip them together:
+        # Highest ranked candidate goes to the highest ranked job.
+        job_pool.sort()
+        candidate_pool.sort()
+        sorted_jobs = [j[1] for j in job_pool]
+        sorted_pool = [c[1] for c in candidate_pool]
+
+        return zip(sorted_pool, sorted_jobs)
+
+
+#--------------------
+# STEP FUNCTION MODEL 
+#--------------------
+class StepFunctionModel
+    def __init__(self):
+        pass
+
+    def simulate_hiring(self, candidates, positions, school_info, ranking='pi_inv', power=2):
+        """ Simulate faculty hiring under the step function model.
+            This model is essentially the configuration model but only for 
+            candidates ranked at least as high as the institution making 
+            the hire. 
+
+            This null model captures the imposed ranking structure
+            from the minimum violations ranking scheme, which is that
+            schools want to avoid violations whenever possible.
+        """
+        worst_ranking = school_info['UNKNOWN'][ranking]
+        
+        # Populate list of available candidates
+        candidate_pool = []
+        noise = 0.1 * np.random.randn(len(candidates))
+        for i, f in enumerate(candidates):
+            place, year = f.phd()
+            try:
+                rank = school_info[place][ranking]
+            except:
+                rank = worst_ranking
+            candidate_pool.append((rank+noise[i], f))
+
+        # Populate list of open jobs
+        job_pool = []
+        noise = 0.1 * np.random.randn(len(positions))
+        for i, s in enumerate(positions):
+            try:
+                rank = school_info[s][ranking]
+            except:
+                rank = worst_ranking
+            job_pool.append((rank+noise[i], s))
+
+   
