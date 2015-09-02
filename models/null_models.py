be9d5bd231d3a74c9f9be9dcd9d5ea0de33e282a
samfway <samfway@gmail.com>
2015-09-02 10:59:50 -0600

    Finished step model

--- models/null_models.py
+++ models/null_models.py
@@ -22,6 +22,7 @@ import numpy as np
     Returns:
         A list of tuples, denoting candidate+position pairs. This is the list of hires made.
 """
+NOISE_LEVEL = 1e-6
 
 # -------------------
 # CONFIGURATION MODEL 
@@ -59,7 +60,7 @@ class BestFirstModel:
 
         # Populate list of available candidates
         candidate_pool = []
-        noise = 0.1 * np.random.randn(len(candidates))
+        noise = NOISE_LEVEL * np.random.randn(len(candidates))
         for i, f in enumerate(candidates:
             place, year = f.phd()
             try:
@@ -95,7 +96,7 @@ class StepFunctionModel
     def __init__(self):
         pass
 
-    def simulate_hiring(self, candidates, positions, school_info, ranking='pi_inv', power=2):
+    def simulate_hiring(self, candidates, positions, school_info, ranking='pi_inv', power=1):
         """ Simulate faculty hiring under the step function model.
             This model is essentially the configuration model but only for 
             candidates ranked at least as high as the institution making 
@@ -104,28 +105,63 @@ class StepFunctionModel
             This null model captures the imposed ranking structure
             from the minimum violations ranking scheme, which is that
             schools want to avoid violations whenever possible.
+
+            If ranking is pi_inv (inverse of PI ranking)... 
+            setting `power' equal to 0, means that all positions are equally likely to be
+            filled first. Setting power to 1 means that positions will be chosen with
+            probability proportional to the inverse of their rank. The larger the power,
+            the more strictly the hiring order resembles the ranks. 
         """
         worst_ranking = school_info['UNKNOWN'][ranking]
+        num_jobs = len(positions)
+        num_candidates = len(candidates)
         
         # Populate list of available candidates
         candidate_pool = []
-        noise = 0.1 * np.random.randn(len(candidates))
+        # Use noise to preallocate ranks and break ties
+        candidate_ranks = NOISE_LEVEL * np.random.randn(num_candidates)
         for i, f in enumerate(candidates):
             place, year = f.phd()
             try:
                 rank = school_info[place][ranking]
             except:
                 rank = worst_ranking
-            candidate_pool.append((rank+noise[i], f))
+            candidate_ranks[i] += rank
+        candidate_pool = zip(candidates, candidate_ranks)
+        remaining_candidates = len(candidate_pool)
 
         # Populate list of open jobs
-        job_pool = []
-        noise = 0.1 * np.random.randn(len(positions))
+        job_ranks = NOISE_LEVEL * np.random.randn(num_jobs)
         for i, s in enumerate(positions):
             try:
                 rank = school_info[s][ranking]
             except:
                 rank = worst_ranking
-            job_pool.append((rank+noise[i], s))
+            job_ranks[i] += rank
 
-   
+        job_ranks = array(job_ranks) ** power
+        job_ranks /= job_ranks.sum()  # make probability
+
+        # Match candidates to jobs
+        for j in xrange(num_jobs):
+            # Select job to fill
+            job_ind = choice(xrange(num_jobs), p=job_ranks)
+            job_rank = job_ranks[job_ind]  # how much probability mass taken out?
+            job_ranks /= (1.-job_rank)  # renormalize
+            job_ranks[job_ind] = 0.  # mark as unavailable
+
+            # Match candidate to job
+            # Select all of the candidates from a school at least as good as the job
+            cand_p = np.array([1 if cand_rank >= job_rank else 1e-9 
+                               for (c, c_rank) in candidate_pool])
+            cand_p /= cand_p.sum()
+            cand_ind = choice(xrange(remaining_candidates), p=cand_p)
+
+            # Log the hire
+            hires.append((candidate_pool[cand_ind], positions[job_ind])
+            
+            # Remove the candidate from the pool
+            del candidate_pool[cand_ind]
+            remaining_candidates -= 1 
+    
+        return hires
