5e7dccff6e7095ec81260412bbbdb559d826374b
samfway <samfway@gmail.com>
2015-09-04 16:01:05 -0600

    Moved step function

--- scripts/compute_error.py
+++ scripts/compute_error.py
@@ -13,7 +13,8 @@ import numpy as np
 from faculty_hiring.parse.load import load_hires_by_year
 from faculty_hiring.parse.institution_parser import parse_institution_records
 from faculty_hiring.models.simulation_engine import SimulationEngine
-from faculty_hiring.models.null_models import ConfigurationModel, BestFirstModel, StepFunctionModel
+from faculty_hiring.models.null_models import ConfigurationModel, BestFirstModel
+from faculty_hiring.models.sigmoid_models import SigmoidModel
 
 
 def interface():
@@ -27,7 +28,6 @@ def interface():
 if __name__=="__main__":
     args = interface()
     
-
     inst = parse_institution_records(open(args.inst_file, 'rU'))
     candidate_pools, job_pools, year_range = load_hires_by_year(open(args.fac_file, 'rU'))
 
@@ -39,11 +39,11 @@ if __name__=="__main__":
     simulator = SimulationEngine(candidate_pools, job_pools, inst, model)
     print 'BestFirst:', simulator.simulate()
 
-    model = StepFunctionModel()
-    simulator = SimulationEngine(candidate_pools, job_pools, inst, model)
+    model = SigmoidModel()
+    simulator = SimulationEngine(candidate_pools, job_pools, inst, model, power=1, prob_function='step')
     print 'Step (pow=1):', simulator.simulate()
 
-    model = StepFunctionModel()
-    simulator = SimulationEngine(candidate_pools, job_pools, inst, model, power=2)
+    model = SigmoidModel()
+    simulator = SimulationEngine(candidate_pools, job_pools, inst, model, power=2, prob_function='step')
     print 'Step (pow=2):', simulator.simulate()
 
