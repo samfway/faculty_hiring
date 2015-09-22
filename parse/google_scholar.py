943af5095d4d5a9004bb719dff7779b71be610f1
samfway <samfway@gmail.com>
2015-09-22 10:31:46 -0600

    Papers can have no title. Added general parsing function

--- parse/google_scholar.py
+++ parse/google_scholar.py
@@ -11,6 +11,9 @@ __status__ = "Development"
 from bs4 import BeautifulSoup
 
 
+GS_FILE = 'GSP_%s_file_%d.html'
+
+
 """
 Parsing of Google scholar pages
 """ 
@@ -23,11 +26,18 @@ def parse_gs_page(html_string):
     
     for pub in pub_listings:
         temp = {}
-        temp['title'] = pub.find('a', {'class':'gsc_a_at'}).text
+        try:
+            temp['title'] = pub.find('a', {'class':'gsc_a_at'}).text
+        except:
+            # Publication without a title? Skip it.
+            continue 
         divs = pub.findAll('div', {'class':'gs_gray'})
         temp['authors'] = divs[0].text
         temp['notes'] = divs[1].text
-        temp['year'] = int(pub.find('td', {'class':'gsc_a_y'}).text)
+        try:
+            temp['year'] = int(pub.find('td', {'class':'gsc_a_y'}).text)
+        except:
+            temp['year'] = 0
         temp['cited'] = pub.find('a', {'class':'gsc_a_ac'}).text
         publications.append(temp)
     
@@ -39,4 +49,21 @@ def parse_gs_page(html_string):
         stats['i10-index'] = int(stats_trs[3].find('td', {'class':'gsc_rsb_std'}).text)
 
     return publications, stats  
+
+def parse_gs_publications(faculty, gs_dir):
+    """ Load all publications into the faculty record """
+    for f in faculty:
+        print f['facultyName']
+        if 'gs' in f:
+            num_loaded = 0
+            filename = os.path.join(gs_dir, GS_FILE % (f['gs'], num_loaded))
+            f['gs_pubs'] = []
+            f['gs_stats'] = None
+
+            while os.path.isfile(filename):
+                pubs, stats = parse_gs_page(open(filename, 'rU').read())
+                f['gs_pubs'] += pubs
+                f['gs_stats'] = stats
+                num_loaded += 1
+                filename = os.path.join(gs_dir, GS_FILE % (f['gs'], num_loaded))
 
