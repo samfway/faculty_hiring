48324c2ea4a3fb1c80e739b3c843580127c15faa
samfway <samfway@gmail.com>
2015-09-26 13:42:47 -0600

    Subfield variables

---
+++ misc/subfield.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+__author__ = "Sam Way"
+__copyright__ = "Copyright 2014, The Clauset Lab"
+__license__ = "BSD"
+__maintainer__ = "Sam Way"
+__email__ = "samfway@gmail.com"
+__status__ = "Development"
+
+
+topic_descriptions = \
+    ['Computer Architecture',
+     'Image/Signal Processing',
+     'Data/Text Mining',
+     'Software Engineering',
+     'AI, Machine Learning',
+     'Communication Networks',
+     'Robotics, HCI, Social Systems',
+     'Theory',
+     'Computational Biology',
+     'Programming Languages']
+
+num_topics = len(topic_descriptions)
