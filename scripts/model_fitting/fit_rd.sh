40a42870099a9171a5fb35aed3bb7374311ca9a0
Sam Way <samfway@gmail.com>
2015-10-02 21:45:31 -0600

    Fittin' scripts

---
+++ scripts/model_fitting/fit_rd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N OPT_rd
+#PBS -joe
+#PBS -t 0-249
+#PBS -q long8gb
+#PBS -l pmem=8gb
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=47:59:59
+FINAL_DIR=/Users/sawa6416/Projects/faculty_hiring/optimization_output/results_files/
+OUTPUT_DIR=/scratch/Users/sawa6416/optimization_output
+TAG=rd
+
+/Users/sawa6416/Projects/faculty_hiring/scripts/optimize.py -i /Users/sawa6416/Projects/faculty_hiring/data/inst_cs.txt -f /Users/sawa6416/Projects/faculty_hiring/data/faculty_cs_0930.txt -p "rd" > ${OUTPUT_DIR}/OPT_${TAG}_${PBS_ARRAYID}.txt
+cat ${OUTPUT_DIR}/OPT_${TAG}_*.txt > ${FINAL_DIR}/RES_${TAG}.txt
---
+++ scripts/model_fitting/fit_rd_pd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N OPT_rd_pd
+#PBS -joe
+#PBS -t 0-249
+#PBS -q long8gb
+#PBS -l pmem=8gb
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=47:59:59
+OUTPUT_DIR=/Users/sawa6416/Projects/faculty_hiring/optimization_output/
+PREFIX=OPT_rd_pd
+/Users/sawa6416/Projects/faculty_hiring/scripts/optimize.py -i /Users/sawa6416/Projects/faculty_hiring/data/inst_cs.txt -f /Users/sawa6416/Projects/faculty_hiring/data/faculty_cs_0930.txt -p "rd_pd" > ${OUTPUT_DIR}/${PREFIX}_${PBS_ARRAYID}.txt
